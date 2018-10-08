858532f726b94f2eac6f079eea7852924a41a0a9
mr-fool <mr-fool@users.noreply.github.com>
2018-10-08 10:58:12 -0600

    Update vertex.glsl

--- hw2/shaders/vertex.glsl
+++ hw2/shaders/vertex.glsl
@@ -29,7 +29,7 @@ void main()
 	//float x = (VertexPosition.x + offset.x); 
 	//float y = (VertexPosition.y + offset.y);
 	vec2 pos= rMatrix * VertexPosition.xy + offset;
-	gl_Position = vec4(pos, 1.0, 0.0);
+	gl_Position = vec4(pos, 1.0, 1.0);
 
     // assign output colour to be interpolated
     uv = VertexUV;
