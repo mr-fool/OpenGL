425a33e8716708282f08cad1264b01b4685857f2
mr-fool <mr-fool@users.noreply.github.com>
2018-11-16 14:58:23 -0700

    Update ray.cpp

--- rebirth/RefactoredBoilerplate/ray.cpp
+++ rebirth/RefactoredBoilerplate/ray.cpp
@@ -7,8 +7,9 @@
 #include <glm/ext.hpp>
 
 
-glm::vec3 color(const ray& r) {
+glm::vec3 ray::color(const ray& r) {
 	glm::vec3 unit_direction = glm::normalize(r.direction());
 	float t = 0.5* (unit_direction.y + 1.0);
 	return (1.0 - t )* glm::vec3(1, 1, 1) + t * glm::vec3(0.5, 0.7, 1.0);
 }
+
