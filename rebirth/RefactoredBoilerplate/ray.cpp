c24ff4e953e45e0991b302c13425c4369ddb92f1
mr-fool <mr-fool@users.noreply.github.com>
2018-11-16 11:25:54 -0700

    Create ray.cpp

---
+++ rebirth/RefactoredBoilerplate/ray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "ray.h"
+
+//**Must include glad and GLFW in this order or it breaks**
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/ext.hpp>
+
+glm::vec3 unit_vector(glm::vec3 v) {
+	return v / v.length();
+}
+glm::vec3 color(const ray& r) {
+	glm::vec3 unit_direction = unit_vector(r.direction());
+	float t = 0.5* (unit_direction.y + 1.0);
+	return (1.0 - t )* glm::vec3(1, 1, 1) + t * glm::vec3(0.5, 0.7, 1.0);
+}
