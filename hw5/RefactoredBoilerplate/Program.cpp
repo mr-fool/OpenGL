4cf869ab4fd7fe74e5da772d6d77e56cbd636e0a
mr-fool <mr-fool@users.noreply.github.com>
2018-11-27 23:22:58 -0700

    removed vbo indexing
    removed vbo indexing

--- hw5/RefactoredBoilerplate/Program.cpp
+++ hw5/RefactoredBoilerplate/Program.cpp
@@ -1,151 +1,125 @@
 /*
- * Scene.cpp
- *
- *  Created on: Sep 10, 2018
- *  Author: John Hall
- */
+* Program.cpp
+*
+*  Created on: Sep 10, 2018
+*      Author: John Hall
+*/
 
-#include "Scene.h"
+#include "Program.h"
 
 #include <iostream>
-
-#include "RenderingEngine.h"
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include <string>
 
 //**Must include glad and GLFW in this order or it breaks**
-#include <glad/glad.h>
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
-#include "texture.h"
-#include <vector>
+#include "RenderingEngine.h"
+#include "Scene.h"
+
 using namespace std;
 using namespace glm;
-#define PI 3.141592653589793238462643383
 
-std::vector<Geometry>& Scene::getObjects(){
-	return this->objects;
+float distScale = 35.0 / 149597870.7; // AU in km
+float radScale = 1.0 / 6378.1; // E in km
+
+Program::Program() {
+	setupWindow();
 }
 
-void Scene::generateSphere(vector<vec3>& positions, vector<vec3>& normals,vector<vec2>& uvs, vector<unsigned int>& indices,vec3 center, float radius, int divisions){
-	objects.clear();
-	rectangle.verts.clear();
-	rectangle.uvs.clear();
-	MyTexture texture;
-	//Load texture uniform
-//Shaders need to be active to load uniforms
-	glUseProgram(renderer->shaderProgram);
-	//Set which texture unit the texture is bound to
-	glActiveTexture(GL_TEXTURE0);
-	//Bind the texture to GL_TEXTURE0
-	glBindTexture(GL_TEXTURE_RECTANGLE, texture.textureID);
-	//Get identifier for uniform
-	GLuint uniformLocation = glGetUniformLocation(renderer->shaderProgram, "imageTexture");
-	//Load texture unit number into uniform
-	glUniform1i(uniformLocation, 0);
-	float step = 1.f / (float)(divisions - 1);
-	float u = 0.f;
+Program::~Program() {
+	//Must be cleaned up in the destructor because these are allocated to the heap
+	delete renderingEngine;
+	delete scene;
+}
 
-	// Traversing the planes of time and space
-	for (int i = 0; i < divisions; i++) {
-		float v = 0.f;
+void Program::start() {
+	renderingEngine = new RenderingEngine();
+	scene = new Scene(renderingEngine);
 
-		//Traversing the planes of time and space (again)
-		for (int j = 0; j < divisions; j++) {
-			vec3 pos = vec3(radius * cos(2.f * PI * u) * sin(PI * v),
-				radius * sin(2.f * PI * u) * sin(PI * v),
-				radius * cos(PI * v)) + center;
-
-			vec3 normal = normalize(pos - center);
-
-			positions.push_back(pos);
-			normals.push_back(normal);
-			uvs.push_back(vec2(u, v));
-
-			v += step;
-		}
-
-		u += step;
+	//Main render loop
+	while (!glfwWindowShouldClose(window)) {
+		scene->displayScene();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
-	for (int i = 0; i < divisions - 1; i++)
-	{
-		for (int j = 0; j < divisions - 1; j++)
-		{
-			unsigned int p00 = i * divisions + j;
-			unsigned int p01 = i * divisions + j + 1;
-			unsigned int p10 = (i + 1) * divisions + j;
-			unsigned int p11 = (i + 1) * divisions + j + 1;
-
-			indices.push_back(p00);
-			indices.push_back(p10);
-			indices.push_back(p01);
-
-			indices.push_back(p01);
-			indices.push_back(p10);
-			indices.push_back(p11);
-		}
-	}
 }
-Scene::Scene(RenderingEngine* renderer) : renderer(renderer) {
 
-	MyTexture texture;
-	InitializeTexture(&texture, "image1-mandrill.png", GL_TEXTURE_RECTANGLE);
-
-	//Load texture uniform
-	//Shaders need to be active to load uniforms
-	glUseProgram(renderer->shaderProgram);
-	//Set which texture unit the texture is bound to
-	glActiveTexture(GL_TEXTURE0);
-	//Bind the texture to GL_TEXTURE0
-	glBindTexture(GL_TEXTURE_RECTANGLE, texture.textureID);
-	//Get identifier for uniform
-	GLuint uniformLocation = glGetUniformLocation(renderer->shaderProgram, "imageTexture");
-	//Load texture unit number into uniform
-	glUniform1i(uniformLocation, 0);
-
-	if(renderer->CheckGLErrors()) {
-		std::cout << "Texture creation failed" << std::endl;
+void Program::setupWindow() {
+	//Initialize the GLFW windowing system
+	if (!glfwInit()) {
+		std::cout << "ERROR: GLFW failed to initialize, TERMINATING" << std::endl;
+		return;
 	}
 
-		// three vertex positions and assocated colours of a triangle
-	rectangle.verts.push_back(glm::vec3( -0.9f, -0.9f, 1.0f));
-	rectangle.verts.push_back(glm::vec3( 0.9f,  -0.9f, 1.0f));
-	rectangle.verts.push_back(glm::vec3( 0.9f, 0.9f, 1.0f));
-	rectangle.verts.push_back(glm::vec3( -0.9f, -0.9f, 1.0f));
-	rectangle.verts.push_back(glm::vec3( 0.9f, 0.9f, 1.0f));
-	rectangle.verts.push_back(glm::vec3( -0.9f, 0.9f, 1.0f));
+	//Set the custom error callback function
+	//Errors will be printed to the console
+	glfwSetErrorCallback(ErrorCallback);
 
+	//Attempt to create a window with an OpenGL 4.1 core profile context
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	int width = 1024;
+	int height = 1024;
+	window = glfwCreateWindow(width, height, "HW5", 0, 0);
+	if (!window) {
+		std::cout << "Program failed to create GLFW window, TERMINATING" << std::endl;
+		glfwTerminate();
+		return;
+	}
 
+	//Set the custom function that tracks key presses
+	glfwSetKeyCallback(window, KeyCallback);
 
-	rectangle.drawMode = GL_TRIANGLES;
+	//Bring the new window to the foreground (not strictly necessary but convenient)
+	glfwMakeContextCurrent(window);
 
-	rectangle.uvs.push_back(glm::vec2( 0.0f, 0.0f));
-	rectangle.uvs.push_back(glm::vec2( float(texture.width), 0.f));
-	rectangle.uvs.push_back(glm::vec2( float(texture.width), float(texture.height)));
-	rectangle.uvs.push_back(glm::vec2( 0.0f, 0.0f));
-	rectangle.uvs.push_back(glm::vec2( float(texture.width), float(texture.height)));
-	rectangle.uvs.push_back(glm::vec2(0.0f, float(texture.height)));
+	//Intialize GLAD (finds appropriate OpenGL configuration for your system)
+	if (!gladLoadGL()) {
+		std::cout << "GLAD init failed" << std::endl;
+		return;
+	}
 
-	//Construct vao and vbos for the triangle
-	RenderingEngine::assignBuffers(rectangle);
-
-	//Send the triangle data to the GPU
-	//Must be done every time the triangle is modified in any way, ex. verts, colors, normals, uvs, etc.
-	RenderingEngine::setBufferData(rectangle);
-
-	//Add the triangle to the scene objects
-	objects.push_back(rectangle);
-
+	//Query and print out information about our OpenGL environment
+	QueryGLVersion();
 }
 
-Scene::~Scene() {
 
+
+void Program::QueryGLVersion() {
+	// query opengl version and renderer information
+	std::string version = reinterpret_cast<const char *>(glGetString(GL_VERSION));
+	std::string glslver = reinterpret_cast<const char *>(glGetString(GL_SHADING_LANGUAGE_VERSION));
+	std::string renderer = reinterpret_cast<const char *>(glGetString(GL_RENDERER));
+
+	std::cout << "OpenGL [ " << version << " ] "
+		<< "with GLSL [ " << glslver << " ] "
+		<< "on renderer [ " << renderer << " ]" << std::endl;
 }
 
-void Scene::displayScene() {
-	renderer->RenderScene(objects);
+void ErrorCallback(int error, const char* description) {
+	std::cout << "GLFW ERROR " << error << ":" << std::endl;
+	std::cout << description << std::endl;
 }
 
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	//Key codes are often prefixed with GLFW_KEY_ and can be found on the GLFW website
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+	Program* program = (Program*)glfwGetWindowUserPointer(window);
+	std::vector<Geometry>& objects = program->getScene()->getObjects();
+	if (key == GLFW_KEY_1 && action == GLFW_PRESS) {
+		vec3 earthCenter = vec3(0.0);
+		vector<vec3> earthPoints;
+		vector<vec3> earthNormals;
+		vector<vec2> earthUvs;
+		vector<unsigned int> earthIndices;
+		earthCenter = vec3(distScale * 149597890, 0.0, 0.0);
+		float earthRadius = pow(radScale * 6378.1, 0.5);
+		program->getScene()->generateSphere(earthPoints, earthNormals, earthUvs, earthIndices, earthCenter, earthRadius, 72);
+	}
+}
