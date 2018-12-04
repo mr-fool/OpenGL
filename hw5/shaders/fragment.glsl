afbc4a397002ee737a6c2c52bf5d46db116ad03a
mr-fool <mr-fool@users.noreply.github.com>
2018-12-04 14:39:21 -0700

    Update fragment.glsl

--- hw5/shaders/fragment.glsl
+++ hw5/shaders/fragment.glsl
@@ -7,16 +7,24 @@
 #version 410
 
 // interpolated colour received from vertex stage
-in vec2 uv;
 
 // first output is mapped to the framebuffer's colour index by default
 out vec4 FragmentColour;
 
-uniform sampler2DRect imageTexture;
+in vec3 FragNormal;
+in vec2 FragUV;
+in vec4 spacePos;
 
-void main(void)
-{
-    // write colour output without modification
-    //FragmentColour = vec4(Colour, 0);
-	FragmentColour = texture(imageTexture, uv);
+uniform sampler2D texSphere;
+uniform bool diffuse;
+
+void main(void) {
+
+	//FragmentColour = vec4((vec3(FragUV, 0.0)), 1);
+	if(diffuse) {
+		vec4 sunColor = vec4(1.0);
+		vec3 lightRay = normalize(vec3(0.0) - spacePos.xyz); // technically this should iterate and use the center of all light objects
+		FragmentColour = texture(texSphere, FragUV) * sunColor * max(0.2, dot(FragNormal, lightRay));
+	}
+	else FragmentColour = texture(texSphere, FragUV);
 }
