3c1e68c11047325ca3a2e4f4212aa9f03063342f
mr-fool <mr-fool@users.noreply.github.com>
2018-12-04 13:19:57 -0700

    Update Program.cpp

--- hw5/RefactoredBoilerplate/Program.cpp
+++ hw5/RefactoredBoilerplate/Program.cpp
@@ -116,7 +116,7 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 	Program* program = (Program*)glfwGetWindowUserPointer(window);
 	std::vector<Geometry>& objects = program->getScene()->getObjects();
 
-	/*if (key == GLFW_KEY_1 && action == GLFW_PRESS) {
+	if (key == GLFW_KEY_1 && action == GLFW_PRESS) {
 		vec3 earthCenter = vec3(0.0);
 		vector<vec3> earthPoints;
 		vector<vec3> earthNormals;
@@ -125,5 +125,5 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 		earthCenter = vec3(distScale * 149597890, 0.0, 0.0);
 		float earthRadius = pow(radScale * 6378.1, 0.5);
 		program->getScene()->generateSphere(earthPoints, earthNormals, earthUvs, earthIndices, earthCenter, earthRadius, 72);
-	}*/
+	}
 }
