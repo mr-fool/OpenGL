173eba81f8602accb035c480f512388c483c3970
mr-fool <mr-fool@users.noreply.github.com>
2018-12-12 10:53:51 -0700

    Update Scene.cpp

--- hw5/RefactoredBoilerplate/Scene.cpp
+++ hw5/RefactoredBoilerplate/Scene.cpp
@@ -31,6 +31,11 @@ using namespace glm;
 std::vector<Geometry>& Scene::getObjects() {
 	return this->objects;
 }
+vec3 getSphereCoords(float u, float v, float center, float radius) {
+	return vec3(radius * cos(2.f * PI * u) * sin(PI * v),
+		radius * sin(2.f * PI * u) * sin(PI * v),
+		radius * cos(PI * v)) + center;
+}
 void Scene::generateSphere(vector<vec3>& positions, vector<vec3>& normals, vector<vec2>& uvs, vector<unsigned int>& indices, vec3 center, float radius, int divisions) {
 	objects.clear();
 	rectangle.verts.clear();
@@ -47,47 +52,77 @@ void Scene::generateSphere(vector<vec3>& positions, vector<vec3>& normals, vecto
 			/*vec3 pos = vec3(radius * cos(2.f * PI * u) * sin(PI * v),
 				radius * sin(2.f * PI * u) * sin(PI * v),
 				radius * cos(PI * v)) + center;*/
+
+			vec3 topLeft = getSphereCoords(u, v, 0, 1);
+			vec3 topRight = getSphereCoords(u + 1, v, 0, 1);
+			vec3 botLeft = getSphereCoords(u, v + 1, 0, 1);
+			vec3 botRight = getSphereCoords(u + 1, v + 1, 0, 1);
 			vec3 pos = vec3(u, v, 0.0f);
 			vec3 normal = normalize(pos - center);
 			//std::cout << "rectangle.verts.push_back " + glm::to_string(pos) << std::endl;
-			rectangle.verts.push_back(pos);
+
+			//rectangle.verts.push_back(pos);
 			//rectangle.verts.push_back(normal);
 			rectangle.uvs.push_back(vec2(u, v));
+
 			pos = vec3(u + step, v, 0.0f);
 			//std::cout << "rectangle.verts.push_back " + glm::to_string(pos) << std::endl;
-			rectangle.verts.push_back(pos);
+
+			//rectangle.verts.push_back(pos);
 			//rectangle.verts.push_back(normal);
 			rectangle.uvs.push_back(vec2(u, v));
+
 			pos = vec3(u, v + step, 0.0f);
 			//std::cout << "rectangle.verts.push_back " + glm::to_string(pos) << std::endl;
-			rectangle.verts.push_back(pos);
+
+			//rectangle.verts.push_back(pos);
 			//rectangle.verts.push_back(normal);
 			rectangle.uvs.push_back(vec2(u, v));
+
 			pos = vec3(u + step, v + step, 0.0f);
 			//std::cout << "rectangle.verts.push_back " + glm::to_string(pos) << std::endl;
-			rectangle.verts.push_back(pos);
+
+			//rectangle.verts.push_back(pos);
 			//rectangle.verts.push_back(normal);
 			rectangle.uvs.push_back(vec2(u, v));
+
 			pos = vec3(u, v + step, 0.0f);
 			//std::cout << "rectangle.verts.push_back " + glm::to_string(pos) << std::endl;
-			rectangle.verts.push_back(pos);
+
+			//rectangle.verts.push_back(pos);
 			//rectangle.verts.push_back(normal);
 			rectangle.uvs.push_back(vec2(u, v));
+
 			pos = vec3(u + step, v, 0.0f);
 			//std::cout << "rectangle.verts.push_back " + glm::to_string(pos) << std::endl;
-			rectangle.verts.push_back(pos);
+
+			//rectangle.verts.push_back(pos);
 			//rectangle.verts.push_back(normal);
+
+
 			rectangle.uvs.push_back(vec2(u, v));
+			rectangle.verts.push_back(topLeft);
+			rectangle.verts.push_back(topRight);
+			rectangle.verts.push_back(botLeft);
+
+			rectangle.verts.push_back(botLeft);
+			rectangle.verts.push_back(topRight);
+			rectangle.verts.push_back(botLeft);
 			v += step;
 		}
+
 		u += step;
+
 	}
+
 	rectangle.drawMode = GL_TRIANGLES;
 	//Construct vao and vbos for the triangle
 	RenderingEngine::assignBuffers(rectangle);
+
 	//Send the triangle data to the GPU
 	//Must be done every time the triangle is modified in any way, ex. verts, colors, normals, uvs, etc.
 	RenderingEngine::setBufferData(rectangle);
+
 	//Add the triangle to the scene objects
 	objects.push_back(rectangle);
 }
@@ -103,7 +138,7 @@ void Scene::generateSphere(vector<vec3>& positions, vector<vec3>& normals, vecto
 	// Load .obj File
 	bool loadout = Loader.LoadFile("sphere.obj");
 	for (int i = 0; i < Loader.LoadedMeshes[0].Vertices.size(); i++) {
-		 cout << Loader.LoadedMeshes[i].Vertices[i].Position.X << endl ;
+		cout << Loader.LoadedMeshes[i].Vertices[i].Position.X << endl;
 	}
 }*/
 Scene::Scene(RenderingEngine* renderer) : renderer(renderer) {
